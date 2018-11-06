532a7ccd4f47f16f8c55b40cb8e9ef5f6a4b3c99
eppoakkoitda <43262437+eppoakkoitda@users.noreply.github.com>
2018-11-06 21:16:23 +0900

    1

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+let localStream = null;
+
+navigator.mediaDevices.getUserMedia({video: true, audio: true})
+    .then(function (stream) {
+        // Success
+        $('#my-video').get(0).srcObject = stream;
+        localStream = stream;
+    }).catch(function (error) {
+        // Error
+        console.error('mediaDevice.getUserMedia() error:', error);
+        return;
+    });
