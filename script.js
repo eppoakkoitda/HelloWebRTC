6695ae09e44a50c092c1bd0b56021217b86a782d
eppoakkoitda <43262437+eppoakkoitda@users.noreply.github.com>
2018-11-11 00:51:57 +0900

    git wakanne

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(function(){
-    
+
 let localStream = null;
 let peer = null;
 let existingCall = null;
@@ -9,7 +9,8 @@ navigator.mediaDevices.getUserMedia({video: true, audio: true})
         // Success
         $('#my-video').get(0).srcObject = stream;
         localStream = stream;
-    }).catch(function (error) {
+    })
+    .catch(function (error) {
         // Error
         console.error('mediaDevice.getUserMedia() error:', error);
         return;
@@ -20,5 +21,5 @@ peer = new Peer({
     key: 'c14010b0-b1a7-4e30-b166-6fb50c553400',
     debug: 3
 });
-    
+
 });
